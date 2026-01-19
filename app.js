c2bcc6de5273d42d841b4269ca2463204f8a3fdd
Pablo Aucapiña <soluciones.pera@gmail.com>
2026-01-19 17:00:36 -0500

    Modify health end point

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/version', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) {
+    throw new Error('Allways fail');
+  }
   res.send('ok');
 });
 
